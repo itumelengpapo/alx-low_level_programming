24009bd9c3ab74219d0e3b12c4a3f3d0cd15d71e
itumelengpapo <itumelengpapo@gmail.com>
2023-02-18 01:01:28 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Printing program that print a line using printf function
- * Return: 0(Successful)
+ * main - Entry
+ * Return: Always 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
